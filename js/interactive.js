49cd2bc85c6dcee15005c709a42f6a3771da4669
Peter <pmank64@gmail.com>
2019-08-03 14:11:33 -0400

    Slide updates, contact button, updated profile image

--- js/interactive.js
+++ js/interactive.js
@@ -7,57 +7,63 @@ $(document).ready(function () {
     var header = $(".header-target");
 	var description = $(".description-target");
 	var image = $(".image-target");
-	var learnMore = ["",
+	var learnMore = ["","","",
 					 "<br><a class='learnMore' href='http://petermankiewich.com/drms.html'><u>Learn More</u></a>",
-					 "<br><a class='learnMore' href='http://moraviadoghaven.com/'><u>The Home Stretch Dog Haven</u></a><br><a class='learnMore' href='https://mfgsourcepros.com/'><u>Manufacturing Source Pros</u></a><br><a class='learnMore' href='http://ithacappella.com/'><u>Ithacappella</u></a>",
+					 "<br><a class='learnMore' href='https://www.bestphoneplans.net/'><u>Best Phone Plans</u></a><br><a class='learnMore' href='http://moraviadoghaven.com/'><u>The Home Stretch Dog Haven</u></a><br><a class='learnMore' href='https://mfgsourcepros.com/'><u>Manufacturing Source Pros</u></a><br><a class='learnMore' href='http://ithacappella.com/'><u>Ithacappella</u></a>",
 					 "<br><a class='learnMore' href='http://petermankiewich.com/impact.html'><u>Learn More</u></a>",
 					 "","","",""];
 
 	
 	
 	
-	var H_0, H_1, H_2, H_3, H_4, H_5, H_6, H_7, H_8;
-	var D_0, D_1, D_2, D_3, D_4, D_5, D_6, D_7, D_8;
+	var H_0, H_1, H_2, H_3, H_4, H_5, H_6, H_7, H_8, H_9, H_10;
+	var D_0, D_1, D_2, D_3, D_4, D_5, D_6, D_7, D_8, D_9, D_10;
 	
 	H_0 = "Hello, I'm Peter Mankiewich";
-	H_1 = "Software and Web Development Intern";
-	H_2 = "Freelance Web Developer";
-	H_3 = "Vice President of Marketing";
-	H_4 = "President, Young Entrepreneurs Organization";
-	H_5 = "Web and Business Development Intern";
-	H_6 = "Web Development Teaching Assistant";
-	H_7 = "Ithaca College Startup Idea Demo Day";
-	H_8 = "Fiverr Seller";
+	H_1 = "Study Abroad Experience in London";
+	H_2 = "Marketing Intern";
+	H_3 = "Software and Web Development Intern";
+	H_4 = "Freelance Web Developer";
+	H_5 = "Vice President of Marketing";
+	H_6 = "President, Young Entrepreneurs Organization";
+	H_7 = "Web and Business Development Intern";
+	H_8 = "Web Development Teaching Assistant";
+	H_9 = "Ithaca College Startup Idea Demo Day";
+	H_10 = "Fiverr Seller";
 	
 	
 	
-	D_0 = "My name is Peter Mankiewich, and I enjoy combining Business and Computer Science to create projects that are effective and high tech. I love business and entrepreneurship, as well as web design and programming. I also enjoy designing graphics, videos and promotional materials using Adobe Creative Cloud. I study my passion at Ithaca College, and incorporate my studies into projects and learning outside of class.";
+	D_0 = "My name is Peter Mankiewich, and I enjoy combining Business and Computer Science to create projects that are effective and high tech. I love business and entrepreneurship, as well as web design and programming. I also enjoy designing graphics, videos and promotional materials using Adobe Creative Cloud. I study my passion at Ithaca College, and incorporate my studies into projects and learning outside of the classroom.";
 	
-	D_1 = "Interned at Disability Reinsurance Management Services as a member of an Agile Software Development Team. I also worked to redesign, rebuild and test the corporate website.";
+	D_1 = "During the Summer of 2019, I had the opporunity to study and intern in London for 6 and a half weeks through CAPA. I took two classes including an urban studies course in which we examined various parts of the city both in the classroom and through field studies. Meeting other students and locals, learning to use public transportation, and creating the perfect cup of tea were just a few things I learned and experienced in London!";
 	
-	D_2 = "Design and develop websites for various organizations and companies. These include a non-profit dog rescue organization, industrial supply startup, and a student-run on campus organization. I have primarily used WordPress, as well as jQuery, CSS and LESS, and backend technologies such as a SQL Database. I've also been learning WebFlow, a user-friendly and powerful website builder.";
+	D_2 = "Interned in East London, working with people with learning disabilites, and marketing online tools that help users develop their communication and organizational skills.";
 	
-	D_3 = "I am the Vice President of Marketing for IC Impact, a student run sustainability based club at Ithaca College. As a club, we focus on bringing guest speakers to campus, and jumpstarting events and workshops that contribute to a more sustainable campus and community.";
+	D_3 = "Interned at Disability Reinsurance Management Services as a member of an Agile Software Development Team. I also worked to redesign, rebuild and test the corporate website.";
 	
-	D_4 = "President of the Young Entrepreneurs Organization, an on-campus club which mentors members to become successful and professional entrepreneurs. Responsibilities include planning and managing club meetings, networking with speakers and alumni, and planning events and off campus trips.";
+	D_4 = "Design and develop websites for various organizations and companies. These include a non-profit dog rescue organization, industrial supply startup, and a student-run on campus organization. I have primarily used WordPress, as well as jQuery, CSS and LESS, and backend technologies such as a SQL Database. I've also been learning WebFlow, a user-friendly and powerful website builder.";
 	
-	D_5 = "Worked at Manor Trade Development Corporation in New York City, developing a lead generation and affiliate website designed to gain sales for the company. The internship involved working in an office environment and collaborating with a team to develop business models and buyer funnels for each website. We used the WordPress platform to develop the websites.";
+	D_5 = "I am the Vice President of Marketing for IC Impact, a student-run sustainability-based club at Ithaca College. As a club, we focus on bringing guest speakers to campus, and jumpstarting events and workshops that contribute to a more sustainable campus and community.";
 	
-	D_6 = "I am currently a teaching assistant for Introduction to Website Development at Ithaca College.  Class time and lab hours are spent helping students with their web programming projects and assignments.";
+	D_6 = "President of the Young Entrepreneurs Organization, an on-campus club which mentors members to become successful and professional entrepreneurs. Responsibilities include planning and managing club meetings, networking with speakers and alumni, and planning events and off-campus trips.";
 	
-	D_7 = "Pitched an eBay application designed to streamline the process of selling products in bulk on eBay. I developed a slide deck, performed research on the target market, and pitched the idea in a four-minute presentation. I won a cash prize to further the business idea. I will be developing this business idea in a small group of student as part of a final project for an Advanced Web Development class at Ithaca College.";
+	D_7 = "Worked at Manor Trade Development Corporation in New York City, developing a lead generation and affiliate website designed to gain sales for the company. The internship involved working in an office environment and collaborating with a team to develop business models and buyer funnels for each website. We used the WordPress platform to develop the websites.";
 	
-	D_8 = "Worked as a seller on Fiverr.com for about two years, selling eBay related services including SEO optimization and product descriptions. I also branched out and worked on video and graphics design projects. I used Adobe Premiere Pro, After Effects, Illustrator and Photoshop to create client projects. I made over 400 sales while maintaining 100% positive feedback, and gaining level 2 seller status.";
+	D_8 = "I am currently a teaching assistant for Introduction to Website Development at Ithaca College.  Class time and lab hours are spent helping students with their web programming projects and assignments. I work with many students who have never been exposed to programning in the past. The class is based in HTML, CSS, and Bootstrap.";
+	
+	D_9 = "Pitched an eBay application designed to streamline the process of selling products in bulk on eBay. I developed a slide deck, performed research on the target market, and pitched the idea in a four-minute presentation. I won a cash prize to further the business idea. I will be developing this business idea in a small group of students as part of a final project for an Advanced Web Development class at Ithaca College.";
+	
+	D_10 = "Worked as a seller on Fiverr.com for about two years, selling eBay related services including SEO optimization and product descriptions. I also branched out and worked on video and graphics design projects. I used Adobe Premiere Pro, After Effects, Illustrator and Photoshop to create client projects. I made over 400 sales while maintaining 100% positive feedback, and gaining level 2 seller status.";
 	
 
 	var right = $(".right-nav");
 	var left = $(".left-nav");
 	
-	var aheader = [H_0,H_1,H_2,H_3,H_4, H_5, H_6, H_7, H_8];
+	var aheader = [H_0,H_1,H_2,H_3,H_4, H_5, H_6, H_7, H_8, H_9, H_10];
 	
-	var adesc = [D_0,D_1,D_2,D_3,D_4, D_5, D_6, D_7, D_8];
+	var adesc = [D_0,D_1,D_2,D_3,D_4, D_5, D_6, D_7, D_8, D_9, D_10];
 	
-	var aimage = ["images/FrontPortrait_BW.png", "images/DRMS.jpg","images/freelance.jpg","images/impact.png", "images/YEOLogo-01.png", "images/nyc.jpg", "images/codeimage-edit.png", "images/Startup_demo_day.jpeg", "images/fiverr.png"];
+	var aimage = ["images/profile_image_2.png","images/london.jpg","images/rix.png", "images/DRMS.jpg","images/freelance.jpg","images/impact.png", "images/YEOLogo-01.png", "images/nyc.jpg", "images/codeimage-edit.png", "images/Startup_demo_day.jpeg", "images/fiverr.png"];
 	
 	var aCircle = [];
 	
@@ -123,6 +129,14 @@ $(document).ready(function () {
 	});
 	$(".circle8").click(function () {
 		num = 7;
+		downArrow();
+	});
+	$(".circle9").click(function () {
+		num = 8;
+		downArrow();
+	});
+	$(".circle10").click(function () {
+		num = 9;
 		downArrow();
 	});
 //-----------------------------------------------------
